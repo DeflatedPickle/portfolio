0bb220f727ab7c6c8cf699481f6442c8764a7012
DeflatedPickle <DeflatedPickle@gmail.com>
2018-10-17 00:39:54 +0100

    Updated the build script

--- upload.py
+++ upload.py
@@ -1,23 +1,44 @@
 import subprocess
 import shutil
 import os
+import sys
+
+import colorama
+
+
+def red(text):
+    return colorama.Fore.RED + text + colorama.Style.RESET_ALL
+
+
+def green(text):
+    return colorama.Fore.GREEN + text + colorama.Style.RESET_ALL
 
 
 if __name__ == "__main__":
+    colorama.init()
+
+    if not shutil.which("git"):
+        print(red("Git could not be found, exiting"))
+        sys.exit()
+
+    if not shutil.which("npm"):
+        print(red("NPM could not be found, exiting"))
+        sys.exit()
+
     # Change to the master branch
-    print("Changing to the master branch...")
+    print(green("Changing to the master branch..."))
     subprocess.call(["git", "checkout", "master"], shell=True)
 
     # Build the site
-    print("Building the files...")
+    print(green("Building the site..."))
     subprocess.call(["npm", "run", "prod"], shell=True)
 
     # Change to the GitHub Pages branch
-    print("Checking out the GitHub Pages branch...")
+    print(green("Checking out the GitHub Pages branch..."))
     subprocess.call(["git", "checkout", "gh-pages"], shell=True)
 
     # Move the files out of dist/
-    print("Moving the files...")
+    print(green("Moving the files..."))
     files = os.listdir("dist")
     for f in files:
         shutil.move("dist/" + f, f)
@@ -25,16 +46,20 @@ if __name__ == "__main__":
     subprocess.call(["git", "add", "."], shell=True)
 
     # Commit the built files
-    print("Commiting the files...")
+    print(green("Committing the files..."))
     subprocess.call(["git", "commit", "-a", "-m", "Rebuilt site"], shell=True)
 
     # Delete the built files
-    print("Deleting the files...")
-    for f in files:
-        os.remove(f)
+    # print("Deleting the files...")
+    # for f in files:
+    #     os.remove(f)
+
+    # Pushing to the GitHub Pages branch
+    print(green("Changing back to the master branch..."))
+    subprocess.call(["git", "push", "origin", "gh-pages"], shell=True)
 
     # Change to the master branch
-    print("Changing back to the master branch...")
+    print(green("Changing back to the master branch..."))
     subprocess.call(["git", "checkout", "master"], shell=True)
 
-    print("Finished uploading the site")
+    print(green("Finished uploading the site, exiting"))
