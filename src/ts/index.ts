22cb5056170c8f8f8da1057348588dd9529d76a0
DeflatedPickle <DeflatedPickle@gmail.com>
2019-11-10 08:20:07 +0000

    Added links to the repos

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,21 +4,33 @@ require("isotope-layout");
 require("chosen-js");
 
 const isotope = new Isotope('#projectGroup', {
-  itemSelector: '.project',
-  layoutMode: 'fitRows'
+    itemSelector: '.project',
+    layoutMode: 'fitRows'
 });
 
 const year = {
-  2017: "seven",
-  2018: "eight",
-  2019: "nine"
+    2016: "six",
+    2017: "seven",
+    2018: "eight",
+    2019: "nine"
+};
+
+const lang = {
+    "Kotlin": "kt",
+    "TypeScript": "ts",
+    "D": "d",
+    "Rust": "rs",
+    "Python": "py",
+    "C++": "cpp",
+    "Perl 6": "pl6",
+    "C": "c",
 };
 
 $("select").chosen({
-  width: "200px",
-  // @ts-ignore
-  no_result_text: "No results..."
+    width: "200px",
+    // @ts-ignore
+    no_result_text: "No results..."
 }).change(() =>
-  // @ts-ignore
-  isotope.arrange({ "filter": $("#years").val().map(it => "." + year[it]).join(' ') + $("#langs").val().map(it => '.' + it).join(' ') })
+    // @ts-ignore
+    isotope.arrange({"filter": $("#years").val().map(it => "." + year[it]).join(' ') + $("#langs").val().map(it => '.' + lang[it.toString()]).join(' ')})
 );
